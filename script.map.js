ed596aee6f3efcf8f37d86b7465384b307918722
DevbratMani01 <parthmani002@gmail.com>
2026-04-21 15:19:34 +0530

    Add files via upload

---
+++ script.map.js
@@ -0,0 +1,7 @@
+let newArray = [1, 2, 3, 4, 5];
+let squaredArray = newArray.map((num) => num ** 2);
+let cubedArray = newArray.map((num) => num ** 3)
+
+console.log(newArray);
+console.log(squaredArray);
+console.log(cubedArray);
