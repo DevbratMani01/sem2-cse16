4775d6799b5bbd19d7c494399edbf1199d92597a
DevbratMani01 <parthmani002@gmail.com>
2026-04-21 15:36:21 +0530

    Add files via upload

---
+++ script.filter.js
@@ -0,0 +1,15 @@
+//Filter 
+
+let evenArr =arr.filter(function(element){
+    return element % 2 ===0;
+});
+ console.log(evenArr);
+
+
+let evenNumbers = [1,2,3,4,5];
+let filteredEvenNumbers = evenNumbers.
+filter(num=>num % 2 === 0);
+console.log(filteredEvenNumbers);
+
+
+
